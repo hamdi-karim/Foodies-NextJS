daf6d71366599a45feae74e69253d553156c64f7
hamdi-karim <k.hamdi1995@gmail.com>
2024-07-03 11:08:11 +0100

    Setting up content for Meals page display meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,27 @@
+import MealsGrid from "@/components/meals/meals-grid";
 import Link from "next/link";
+
+import classes from "./page.module.css";
 
 export default function MealsPage() {
   return (
-    <main>
-      <h2>Welcome to Meals Page</h2>
-      <Link href="/meals/share">Share</Link>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, cleated{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+
+        <p>Choose your favorite recipe, and have fun cooking it</p>
+
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
