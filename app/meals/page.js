80a4ff7ea31f171263a14019c98f61a7725f1067
hamdi-karim <k.hamdi1995@gmail.com>
2024-07-03 13:02:18 +0100

    outsourcing loading code in Meals component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,15 @@
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 import Link from "next/link";
+import { Suspense } from "react";
 
 import classes from "./page.module.css";
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
   return (
@@ -20,7 +28,11 @@ export default function MealsPage() {
       </header>
 
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
