3cbe073700b8bdfe64d7b5effe3186958ed5cbcd
hamdi-karim <k.hamdi1995@gmail.com>
2024-07-16 09:55:31 +0100

    redirect meals wrong paths to the closest not found page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,10 +1,15 @@
 import { getMeal } from "@/lib/meals";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 import classes from "./page.module.css";
 
 export default async function MealsDetailsPage({ params }) {
   const meal = await getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
