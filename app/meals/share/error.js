bcc7252f53b5de30884710c6150bbe4d2772ba46
hamdi-karim <k.hamdi1995@gmail.com>
2024-07-28 13:04:25 +0100

    Adding server-side input validation in share meals form page by throwing an error.js file

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ErrorPage({ error }) {
+  return (
+    <div className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to create a new meal! .</p>
+    </div>
+  );
+}
